20ec2714496386584b23a32088061f60a3006736
Aaron Hooper <rnjckhpr@gmail.com>
2022-02-01 17:58:07 +0000

    Lint

--- roman.rb
+++ roman.rb
@@ -1,30 +1,26 @@
 module Roman
-    ARABIC = [
-        1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
-    ]
-    
-    ROMAN = [
-        "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
-    ]
-    
-    def Roman.from_arabic(arabic)
-        remainder = arabic
-        index = 0
-        out = ""
-        
-        while remainder >= 0 do
-            if remainder == 0
-                return out 
-            end
-            
-            if remainder - ARABIC[index] >= 0
-                out += ROMAN[index]
-                remainder -= ARABIC[index]
-            end
-            
-            if remainder - ARABIC[index] < 0
-                index += 1
-            end
-        end
+  ARABIC = [
+    1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
+  ]
+
+  ROMAN = %w[
+    M CM D CD C XC L XL X IX V IV I
+  ]
+
+  def self.from_arabic(arabic)
+    remainder = arabic
+    index = 0
+    out = ''
+
+    while remainder >= 0
+      return out if remainder == 0
+
+      if remainder - ARABIC[index] >= 0
+        out += ROMAN[index]
+        remainder -= ARABIC[index]
+      end
+
+      index += 1 if remainder - ARABIC[index] < 0
     end
+  end
 end
