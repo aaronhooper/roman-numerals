122b0e1a8817c738d9b4b899f05903f8c239ee0f
Aaron Hooper <rnjckhpr@gmail.com>
2022-02-01 18:22:48 +0000

    Refactor

--- roman.rb
+++ roman.rb
@@ -1,11 +1,8 @@
-module Roman
-  ARABIC = [
-    1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
-  ]
+# frozen_string_literal: true
 
-  ROMAN = %w[
-    M CM D CD C XC L XL X IX V IV I
-  ]
+module Roman
+  ROMAN = %w[M CM D CD C XC L XL X IX V IV I]
+  ARABIC = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
 
   def self.from_arabic(arabic)
     remainder = arabic
@@ -13,14 +10,14 @@ module Roman
     out = ''
 
     while remainder >= 0
-      return out if remainder == 0
+      return out if remainder.zero?
 
       if remainder - ARABIC[index] >= 0
         out += ROMAN[index]
         remainder -= ARABIC[index]
       end
 
-      index += 1 if remainder - ARABIC[index] < 0
+      index += 1 if (remainder - ARABIC[index]).negative?
     end
   end
 end
